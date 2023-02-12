4bca168925d965550b5eaf7b0b9c23e4d0b0e2eb
woprandis09 <bkpecho05@gmail.com>
2023-02-12 22:35:07 +0800

    version 1.0 Done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,48 @@
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
 
+const rating = [one, two, three, four, five];
+
+rating.forEach(function changeColor(number) {
+  number.addEventListener('click', function () {
+    rating.forEach(function resetColor(number) {
+      number.classList.remove('selected');
+    });
+    this.classList.add('selected');
+  });
+});
+
+document.querySelector('button').addEventListener('click', showThankYou);
+
+let selectedValue;
+
+rating.forEach(function (number) {
+  number.addEventListener('click', function () {
+    rating.forEach(function (number) {
+      number.classList.remove('selected');
+    });
+    this.classList.add('selected');
+    selectedValue = this.textContent;
+  });
+});
+
+function showThankYou() {
+  if (selectedValue) {
+    const ratingComponent = document.querySelector('main');
+    const thankYouComponent = document.querySelector('.thank-you');
+    const showSelectedValue = document.querySelector('#rating-number');
+
+    ratingComponent.style.display = 'none';
+    thankYouComponent.style.display = 'block';
+    showSelectedValue.textContent = ` ${selectedValue} `;
+  } else {
+    const button = document.querySelector('button');
+    button.classList.add('wiggle');
+    button.addEventListener('animationend', function () {
+      button.classList.remove('wiggle');
+    });
+  }
+}
